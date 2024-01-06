bec6fc390f966678d369190f338a344e93a6af25
MrGod1y <Devom.b@yahoo.com>
2024-01-06 00:50:18 -0800

    Add files via upload
    Finished PEMDAS (Parenthesis, Exponent, Multiplication, Division, Addition, Subtraction)

--- Advanced Calc w JSXGraph - Active/CalcEval.js
+++ Advanced Calc w JSXGraph - Active/CalcEval.js
@@ -54,11 +54,123 @@ function stringParser_and_evaluator (elementID) {
           PowEval = Math.pow(leftSideNum,rightSideNum)
           string.replace(string.substring(string.substring.indexOf("^")-leftSideNum.length,string.substring("^")+rightSideNum.length),String(PowEval))
         }
-
+        // Expo Function Eval and replace
         while (!string.substring(lastOpenPara,lastClosePara+1).includes("^") && string.substring(lastOpenPara,lastClosePara+1).includes("/")){
-          // Trying to follow PEMDAS
-          // Unfinished
+          let leftSideNum
+          let rightSideNum
+          let indexPow = string.substring(lastOpenPara,lastClosePara+1).indexOf("/")
+
+          for (let i=indexPow-1;i <= 0;i--) { //Left side
+            // This for loop should loop back indexes from indexPow and check if every index before that is a num and add it to leftSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              leftSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+              } else {
+                break
+              }
+          }
+
+          for (let i=indexPow+1;i >= string.substring(lastOpenPara,lastClosePara+1).length;i++) { // Right side
+            // This for loop should loop forward indexes from indexPow and check if every index after that is a num and add it to rightSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              rightSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+            } else {
+              break
+            }
+          }
+
+          leftSideNum = Number(leftSideNum.reverse().join(''))
+          rightSideNum = Number(rightSideNum.join(''))
+          DivEval = leftSideNum/rightSideNum
+          string.replace(string.substring(string.substring.indexOf("/")-leftSideNum.length,string.substring("/")+rightSideNum.length),String(DivEval))
         }
+        //Division Function Eval and replace
+        while (!string.substring(lastOpenPara,lastClosePara+1).includes("^") && !string.substring(lastOpenPara,lastClosePara+1).includes("/") && string.substring(lastOpenPara,lastClosePara+1).includes("*")){
+          let leftSideNum
+          let rightSideNum
+          let indexPow = string.substring(lastOpenPara,lastClosePara+1).indexOf("*")
+
+          for (let i=indexPow-1;i <= 0;i--) { //Left side
+            // This for loop should loop back indexes from indexPow and check if every index before that is a num and add it to leftSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              leftSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+              } else {
+                break
+              }
+          }
+
+          for (let i=indexPow+1;i >= string.substring(lastOpenPara,lastClosePara+1).length;i++) { // Right side
+            // This for loop should loop forward indexes from indexPow and check if every index after that is a num and add it to rightSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              rightSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+            } else {
+              break
+            }
+          }
+
+          leftSideNum = Number(leftSideNum.reverse().join(''))
+          rightSideNum = Number(rightSideNum.join(''))
+          MultEval = leftSideNum*rightSideNum
+          string.replace(string.substring(string.substring.indexOf("*")-leftSideNum.length,string.substring("*")+rightSideNum.length),String(MultEval))
+        }
+        // Multiplication Function Eval and replace
+        while (!string.substring(lastOpenPara,lastClosePara+1).includes("^") && !string.substring(lastOpenPara,lastClosePara+1).includes("/") && !string.substring(lastOpenPara,lastClosePara+1).includes("*") && string.substring(lastOpenPara,lastClosePara+1).includes("+")){
+          let leftSideNum
+          let rightSideNum
+          let indexPow = string.substring(lastOpenPara,lastClosePara+1).indexOf("+")
+
+          for (let i=indexPow-1;i <= 0;i--) { //Left side
+            // This for loop should loop back indexes from indexPow and check if every index before that is a num and add it to leftSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              leftSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+              } else {
+                break
+              }
+          }
+
+          for (let i=indexPow+1;i >= string.substring(lastOpenPara,lastClosePara+1).length;i++) { // Right side
+            // This for loop should loop forward indexes from indexPow and check if every index after that is a num and add it to rightSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              rightSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+            } else {
+              break
+            }
+          }
+
+          leftSideNum = Number(leftSideNum.reverse().join(''))
+          rightSideNum = Number(rightSideNum.join(''))
+          AddEval = leftSideNum+rightSideNum
+          string.replace(string.substring(string.substring.indexOf("+")-leftSideNum.length,string.substring("+")+rightSideNum.length),String(AddEval))
+        }
+        // Addition Function Eval and repalce
+        while (!string.substring(lastOpenPara,lastClosePara+1).includes("^") && !string.substring(lastOpenPara,lastClosePara+1).includes("/") && !string.substring(lastOpenPara,lastClosePara+1).includes("*") && !string.substring(lastOpenPara,lastClosePara+1).includes("+") && string.substring(lastOpenPara,lastClosePara+1).includes("-")){
+          let leftSideNum
+          let rightSideNum
+          let indexPow = string.substring(lastOpenPara,lastClosePara+1).indexOf("-")
+
+          for (let i=indexPow-1;i <= 0;i--) { //Left side
+            // This for loop should loop back indexes from indexPow and check if every index before that is a num and add it to leftSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              leftSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+              } else {
+                break
+              }
+          }
+
+          for (let i=indexPow+1;i >= string.substring(lastOpenPara,lastClosePara+1).length;i++) { // Right side
+            // This for loop should loop forward indexes from indexPow and check if every index after that is a num and add it to rightSide array if not a num it should cut the array then turn it into a string using toString()
+            if (Number.isInteger(string.substring(lastOpenPara,lastClosePara+1)[i])){
+              rightSideNum.push(string.substring(lastOpenPara,lastClosePara+1)[i])
+            } else {
+              break
+            }
+          }
+
+          leftSideNum = Number(leftSideNum.reverse().join(''))
+          rightSideNum = Number(rightSideNum.join(''))
+          SubEval = leftSideNum-rightSideNum
+          string.replace(string.substring(string.substring.indexOf("-")-leftSideNum.length,string.substring("-")+rightSideNum.length),String(AddEval))
+        }
+        // Subtraction Function Eval and repalce
       }
     }
   }
